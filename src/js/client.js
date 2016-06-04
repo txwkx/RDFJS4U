9a3c4a8d30f0df3b04826b7ff5558ebd267b9322
Todor Tsankov <tsankov.todya@gmail.com>
2016-06-04 19:18:41 +0200

    React router navigation added

--- src/js/client.js
+++ src/js/client.js
@@ -1,1 +1,22 @@
-console.log('Compiled!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+import About from './pages/About';
+import Documentation from './pages/Documentation';
+import Layout from './pages/Layout';
+import Libraries from './pages/Libraries';
+
+const app = document.getElementById('app');
+
+const route = (
+  <Router history={hashHistory}>
+    <Route path='/' component={Layout}>
+      <IndexRoute component={Libraries}></IndexRoute>
+      <Route path='documentation' name="documentation" component={Documentation}></Route>
+      <Route path='about' name="about" component={About}></Route>
+    </Route>
+  </Router>
+);
+
+ReactDOM.render(route, app);
