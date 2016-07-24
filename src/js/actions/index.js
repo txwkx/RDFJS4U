af20389bd9cb90293cd407cd0176416c0a7e75e5
Todor Tsankov <tsankov.todya@gmail.com>
2016-07-24 17:43:44 +0200

    Updated Action creator
    - Added getTableHeaderList
    - Added getFeatureSets

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -2,6 +2,8 @@ import firebase from 'firebase';
 import _ from 'lodash';
 
 export const GET_TABLE_TABS = 'GET_TABLE_TABS';
+export const GET_TABLE_HEADER = 'GET_TABLE_HEADER';
+export const GET_FEATURE_SETS = 'GET_FEATURE_SETS';
 
 const config = {
     apiKey: 'AIzaSyDm7TPKpzOPvcAgqwA4X5v0iTWN3wmam6o',
@@ -18,6 +20,28 @@ export function getTableTabs() {
     rootRef.child('tabletabs').once('value', snap => {
       dispatch({
         type: GET_TABLE_TABS,
+        payload: snap.val()
+      });
+    });
+  };
+}
+
+export function getTableHeaderList() {
+  return (dispatch) => {
+    rootRef.child('theaderList').once('value', snap => {
+      dispatch({
+        type: GET_TABLE_HEADER,
+        payload: snap.val()
+      });
+    });
+  };
+}
+
+export function getFeatureSets(){
+  return (dispatch) => {
+    rootRef.child('featuresSets').once('value', snap => {
+      dispatch({
+        type: GET_FEATURE_SETS,
         payload: snap.val()
       });
     });
