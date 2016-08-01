0b04b31cbfb4637cc317d4dc5eeb8e9bd48e4c2c
Todor Tsankov <tsankov.todya@gmail.com>
2016-08-01 22:55:59 +0200

    Wired up Redux store and middleware

--- src/js/client.js
+++ src/js/client.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import About from './pages/About';
 import Documentation from './pages/Documentation';
 import Layout from './pages/Layout';
 import Libraries from './pages/Libraries';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 const app = document.getElementById('app');
 
-const route = (
+const provider = (
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={hashHistory}>
+      <Route path='/' component={Layout}>
+        <IndexRoute component={Libraries}></IndexRoute>
+        <Route path='documentation' name="documentation" component={Documentation}></Route>
+        <Route path='about' name="about" component={About}></Route>
+      </Route>
+    </Router>
+  </Provider>
+);
+
+/*const route = (
   <Router history={hashHistory}>
     <Route path='/' component={Layout}>
       <IndexRoute component={Libraries}></IndexRoute>
@@ -17,6 +35,6 @@ const route = (
       <Route path='about' name="about" component={About}></Route>
     </Route>
   </Router>
-);
+);*/
 
-ReactDOM.render(route, app);
+ReactDOM.render(provider, app);
