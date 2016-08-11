2d0a216d4d8439b2637ba72f4ae4fb628a4ccb18
Todor Tsankov <tsankov.todya@gmail.com>
2016-08-11 23:54:51 +0200

    minor cleanup

--- src/js/client.js
+++ src/js/client.js
@@ -27,14 +27,4 @@ const provider = (
   </Provider>
 );
 
-/*const route = (
-  <Router history={hashHistory}>
-    <Route path='/' component={Layout}>
-      <IndexRoute component={Libraries}></IndexRoute>
-      <Route path='documentation' name="documentation" component={Documentation}></Route>
-      <Route path='about' name="about" component={About}></Route>
-    </Route>
-  </Router>
-);*/
-
 ReactDOM.render(provider, app);
