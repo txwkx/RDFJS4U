65d424f3b22182ea9100e0eef89e2fe00b087cfd
Todor Tsankov <tsankov.todya@gmail.com>
2016-09-19 13:57:25 +0200

    Added Query_Table reducer

--- src/js/reducers/reducer_libtable.js
+++ src/js/reducers/reducer_libtable.js
@@ -1,14 +1,16 @@
-import { GET_TABLE_TABS, GET_TABLE_HEADER, GET_TABLE_CONTENT } from '../actions/index';
+import * as actions from '../actions/index';
 
 const INITIAL_STATE = {tabs: [], headers: [], content: []};
 
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case GET_TABLE_TABS:
+    case actions.GET_TABLE_TABS:
       return {...state, tabs: action.payload};
-    case GET_TABLE_HEADER:
+    case actions.GET_TABLE_HEADER:
       return {...state, headers: action.payload};
-    case GET_TABLE_CONTENT:
+    case actions.GET_TABLE_CONTENT:
+      return {...state, content: action.payload};
+    case actions.QUERY_TABLE:
       return {...state, content: action.payload};
   }
 
