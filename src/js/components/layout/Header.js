006e419c1e5ca0f8915fcf3905664a3d65dfc83d
Todor Tsankov <tsankov.todya@gmail.com>
2016-09-28 18:11:40 +0200

    Refactored Header and PageTitle components

--- src/js/components/layout/Header.js
+++ src/js/components/layout/Header.js
@@ -2,33 +2,26 @@ import React from 'react';
 
 import { IndexLink, Link } from 'react-router';
 
-class Header extends React.Component {
+const Header = ({ location }) => {
 
-  render() {
-    const { location } = this.props;
-    const librariesClass = location.pathname === '/' ? 'open' : '';
-    const documentationClass = location.pathname.match(/^\/documentation/) ? 'open' : '';
-    const aboutClass = location.pathname.match(/^\/about/) ? 'open' : '';
-
-    return (
-      <nav class='navbar-fixed-top' role='navigation'>
-        <div class='navbar-header'>
-          <a class='navbar-brand'>RDFJS 4 U</a>
-        </div>
-        <ul class='nav navbar-right top-nav'>
-          <li class={librariesClass}>
-            <IndexLink to='/'><i class='fa fa-bar-chart-o'></i> Libraries</IndexLink>
-          </li>
-          <li class={documentationClass}>
-            <Link to='documentation'><i class='fa fa-fw fa-file'></i> Documentation</Link>
-          </li>
-          <li class={aboutClass}>
-            <Link to='about'><i class='fa fa-user'></i> About</Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+  return (
+    <nav class='navbar-fixed-top' role='navigation'>
+      <div class='navbar-header'>
+        <a class='navbar-brand'>RDFJS 4 U</a>
+      </div>
+      <ul class='nav navbar-right top-nav'>
+        <li class={`${location.pathname === '/' ? 'open' : ''}`}>
+          <IndexLink to='/'><i class='fa fa-bar-chart-o'></i> Libraries</IndexLink>
+        </li>
+        <li class={`${location.pathname.match(/^\/documentation/) ? 'open' : ''}`}>
+          <Link to='documentation'><i class='fa fa-fw fa-file'></i> Documentation</Link>
+        </li>
+        <li class={`${location.pathname.match(/^\/about/) ? 'open' : ''}`}>
+          <Link to='about'><i class='fa fa-user'></i> About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 
 }
 
